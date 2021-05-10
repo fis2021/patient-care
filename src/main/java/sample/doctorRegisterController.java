59a2e0dc76b3f780202619a2fa63db571580620b
Bogdan Lacatusu <ionut.bogdy00@gmail.com>
2021-05-10 17:44:13 +0300

    Add new doctor to db

--- src/main/java/sample/doctorRegisterController.java
+++ src/main/java/sample/doctorRegisterController.java
@@ -4,13 +4,12 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import sample.services.UserService;
 
 import java.io.File;
 import java.net.URL;
@@ -42,6 +41,13 @@ public class doctorRegisterController implements Initializable {
     private TextField pnTextField;
     @FXML
     private TextField specializationTextField;
+    @FXML
+    private RadioButton genderM;
+    @FXML
+    private RadioButton genderF;
+    @FXML
+    private RadioButton genderO;
+
 
 
     @Override
@@ -51,7 +57,7 @@ public class doctorRegisterController implements Initializable {
         Image registerImage = new Image(registerFile.toURI().toString());
         registerImageView.setImage(registerImage);
     }
-        public void cancelButtonOnAction (ActionEvent event) {
+    public void cancelButtonOnAction (ActionEvent event) {
         Stage stage = (Stage) cancelButton.getScene().getWindow();
         stage.close();
     }
@@ -59,16 +65,22 @@ public class doctorRegisterController implements Initializable {
     public void registerButtonOnAction (ActionEvent event) {
 
         if (setPasswordField.getText().equals(confirmPasswordField.getText())) {
-        registerUser();
-        confirmPasswordLabel.setText("");
-        registrationLabelMessage.setText("User has been registered successfully!");
-    } else
+            registerUser();
+            confirmPasswordLabel.setText("");
+            registrationLabelMessage.setText("User has been registered successfully!");
+        } else
             confirmPasswordLabel.setText("Password does not match");
         if(fnameTextField.getText().isBlank() || lnameTextField.getText().isBlank() ||  usernameTextField.getText().isBlank() || emailTextField.getText().isBlank() || pnTextField.getText().isBlank() || specializationTextField.getText().isBlank()) {
             registrationLabelMessage.setText("");
         }
-}
-     public void registerUser (){
+    }
+    public void registerUser (){
+        String gender="";
+        if(genderM.isSelected()) gender="Male";
+        else if(genderF.isSelected()) gender="Female";
+        else if(genderO.isSelected()) gender="Other";
+        UserService.addDoctor(fnameTextField.getText(), lnameTextField.getText(), pnTextField.getText(), specializationTextField.getText(),usernameTextField.getText(),setPasswordField.getText(),emailTextField.getText(), gender);
 
-     }
+
+    }
 }
