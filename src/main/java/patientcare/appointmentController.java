c69be34b6aab3d5cfcf71b1fbad6d0392b4ef826
andreeamihai2001 <80961585+andreeamihai2001@users.noreply.github.com>
2021-05-11 15:51:40 +0300

    PC-68 Started the database for the appointments

--- src/main/java/patientcare/appointmentController.java
+++ src/main/java/patientcare/appointmentController.java
@@ -7,13 +7,19 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import patientcare.services.AppointmentService;
+import patientcare.services.UserService;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 public class appointmentController implements Initializable {
@@ -24,7 +30,21 @@ public class appointmentController implements Initializable {
     private Button cancelButton;
     @FXML
     private ImageView infoImageView;
+    @FXML
+    private DatePicker dateField;
+    @FXML
+    private Pane[] appointmentButtons;
+    @FXML
+    private Label[] appointmentHours;
 
+    public void setAppointmentButtons(Pane[] appointmentButtons) {
+        for(int i=0; i<14;i++)
+        appointmentButtons[i] = new Pane();
+    }
+    public void setAppointmentHours(Label[] appointmentHours) {
+        for(int i=0; i<14;i++)
+            appointmentHours[i] = new Label();
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         File infoFile = new File("images/info_icon.png");
@@ -42,6 +62,12 @@ public class appointmentController implements Initializable {
     public void cancelButtonOnAction (ActionEvent event) {
         Stage stage = (Stage) cancelButton.getScene().getWindow();
         stage.close();
+    }
+    public void registerAppointment (){
+
+        AppointmentService.addAppointment(dateField.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")),);
+
+
     }
 
 }
