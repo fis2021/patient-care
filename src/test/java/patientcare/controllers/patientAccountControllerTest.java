cba20dca506f657048054cb35cb93b7afc390e3f
andreeamihai2001 <80961585+andreeamihai2001@users.noreply.github.com>
2021-05-16 21:21:45 +0300

    PC-77 Made test for patient account controller

---
+++ src/test/java/patientcare/controllers/patientAccountControllerTest.java
@@ -0,0 +1,12 @@
+package patientcare.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class patientAccountControllerTest {
+
+    @Test
+    public void paAccountController(){
+
+    }
+}
